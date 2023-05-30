5927ae2d1fc5a699ad6e034b76069df4482dac58
anns318 <an310898@gmail.com>
2023-05-31 02:29:41 +0700

    update admin template

--- admin/js/layerForm.js
+++
@@ -1,8 +0,0 @@
-function showLayerForm() {
-  document.querySelector("#layer-form").style.display = "flex";
-}
-function hideLayerForm() {
-  document.querySelector("#layer-form").style.display = "none";
-  document.querySelector("#formAddNew").style.display = "none";
-  document.querySelector("#formEdit").style.display = "none";
-}
