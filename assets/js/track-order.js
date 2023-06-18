09749e0beeb6b4343297290d82251fe7d0ac0a70
anns318 <an310898@gmai.com>
2023-06-19 03:08:08 +0700

    add track order

---
+++ assets/js/track-order.js
@@ -0,0 +1,144 @@
+$('document').ready(function () {
+    $('form').submit(function (e) {
+        e.preventDefault();
+        return false;
+    })
+})
+
+
+async function trackOrder() {
+    $('#alert-input').text('')
+
+    const input = $('#inputTrack').val().trim()
+
+    if (input.length === 0) {
+        $('#alert-input').text('Please input your customer id or phone number')
+        return
+    }
+    let data
+    if (input[0] === 'B' || input[0] === 'L' || input[0] === 'D') {
+
+        const formData = {
+            Customer: input,
+            IsPhoneNumber: 0
+        }
+
+        data = await fetchDynamicAPI('getOrderDetailByCustomer', formData)
+
+    } else {
+
+        const formData = {
+            Customer: input,
+            IsPhoneNumber: 1
+        }
+        data = await fetchDynamicAPI('getOrderDetailByCustomer', formData)
+        if (data.length === 0) {
+            $('#alert-input').text('Wrong customer id or phone number')
+            return
+        }
+
+    }
+    if (data.length === 0) {
+        $('#alert-input').text('Wrong customer id or phone number')
+        return
+    }
+    console.log("🚀 ~ file: track-order.js:27 ~ trackOrder ~ data:", data)
+    const html = data.map(x => {
+        return `
+            <div class="card">
+                                <div class="card-header d-flex card-header-content" data-toggle="collapse" data-target="#Order${x.OrderId}" aria-expanded="true" aria-controls="collapseOne" >
+                                    <h2 class="mb-0">
+                                        <button class="btn btn-link" type="button" >
+                                            ${x.CustomerID}
+                                        </button>
+                                    </h2>
+                                    <div>
+                                        Status:Pending
+                                    </div>
+                                </div>
+                                <div id="Order${x.OrderId}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                    <div class="card-body" id="order-info">
+                                        <div class="form-group row">
+                                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                                <div class="d-flex">
+                                                    <label>First Name:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.FirstName}</div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <div class="d-flex">
+                                                    <label>Last Name:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.LastName}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="d-flex">
+                                                <label>Phone:</label>
+                                                <div class="flex-grow-1 text-center info-border-bottom">${x.Phone}</div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div>
+                                                <div class="d-flex"><label>Address:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.Address}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div>
+                                                <div class="d-flex"><label>Connection Type:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.ConnectionType}</div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div class="form-group">
+                                            <div>
+                                                <div class="d-flex"><label>Connection Option:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.OptionName}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div>
+                                                <div class="d-flex"><label>Connection Detail:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.Description}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div>
+                                                <div class="d-flex">
+                                                    <label>Product :</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.ProductName}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div>
+                                                <div class="d-flex"><label>Order Date:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">${x.OrderDate}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                                <div class="d-flex"><label>One Time Charge:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom">$${x.BillAmount}</div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <div class="d-flex"><label>Motnhly Charge:</label>
+                                                    <div class="flex-grow-1 text-center info-border-bottom"> $${(x.Price * 87.5 / 100).toFixed(2)}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+        `
+    }).join('')
+
+    $('#accordionExample').html(html)
+}
